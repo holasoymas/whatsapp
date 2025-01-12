5526a555c526fd3b8712d5dfdf297aa08af93e61
holasoymas <71905956331mahesh@gmail.com>
2025-01-12 22:22:08 +0545

    app entry point for index.js file

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import path from "node:path";
+import * as Middleware from "./utils/middleware.js";
+import { loginRoute } from "./routes/loginRoute.js";
+import { chatRoute } from "./routes/chatRoute.js";
+
+const app = express();
+
+//!NOTE: middleware : parse the incoming json request body, convert it into js object
+
+// withot it res.body would be undefined
+app.use(express.json());
+
+app.use(Middleware.requestLogger);
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "public")));
+
+//!NOTE: Route mmiddleware
+app.use("/login", loginRoute);
+
+app.use("/chat", chatRoute);
+//!NOTE: if none of the route match execute this
+app.use(Middleware.unknownEndpoint);
+
+//!NOTE: if any error occurs during the req,res execute this middleware
+app.use(Middleware.errorHandler);
+
+export default app;
