9e2fe9791f8f6af25eb01a3ac1b96d211fd5b837
holasoymas <71905956331mahesh@gmail.com>
2025-01-12 22:25:51 +0545

    moved the fetcher function from backnd to frontend

---
+++ public/js/utils/fetcher.js
@@ -0,0 +1,40 @@
+const Config = { BASE_API_URL: "http://localhost:8000" };
+
+export async function fetchFromServer(endpoint, method, body = null, authToken = null) {
+  try {
+    const response = await fetch(
+      `${Config.BASE_API_URL}/${endpoint}`,
+      buildOptions(method, body, authToken),
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.log(errorData);
+      const resStatus = response.status;
+      return { status: resStatus, error: errorData };
+    }
+    const result = await response.json();
+    console.log(result);
+    return { status: 200, result };
+  } catch (error) {
+    // console.error("Fetch error:", error);
+    throw error;
+  }
+}
+
+function buildOptions(method, body, authToken) {
+  const options = {};
+
+  options.method = method;
+
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
+
+  options.headers = {
+    "Content-Type": "application/json; charset=UTF-8",
+    Authorization: `Bearer ${authToken}`,
+  };
+
+  return options;
+}
