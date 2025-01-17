f12a84a76aa0623be0033ae9e77bf685958be01a
holasoymas <71905956331mahesh@gmail.com>
2025-01-17 17:24:34 +0545

    added cookieparser middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import path from "node:path";
 import * as Middleware from "./utils/middleware.js";
 import { loginRoute } from "./routes/loginRoute.js";
 import { chatRoute } from "./routes/chatRoute.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -10,6 +11,9 @@ const app = express();
 
 // withot it res.body would be undefined
 app.use(express.json());
+
+//middleware for cookie parsing
+app.use(cookieParser());
 
 app.use(Middleware.requestLogger);
 const __dirname = path.resolve();
