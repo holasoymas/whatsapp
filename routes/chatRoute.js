15b40797de81e4fe3713853af9538fe27d003b42
holasoymas <71905956331mahesh@gmail.com>
2025-01-27 13:55:23 +0545

    added routes for searching and creating connections

--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -5,6 +5,11 @@ const chatRoute = express.Router();
 chatRoute.get("", ChatControllers.getChatRoute);
 chatRoute.get("/getChats", ChatControllers.getChats);
 
+// search user on searchbar
+chatRoute.get("/getUsers", ChatControllers.getUsersContains);
+
+chatRoute.post("/createOrGetConnection", ChatControllers.createOrGetConnection);
+
 chatRoute.get("/getMessages/:chatId", ChatControllers.getMessages);
 chatRoute.post("/sendMessages", ChatControllers.sendMessages);
 
